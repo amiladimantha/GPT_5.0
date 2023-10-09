623f051de9ed832a5b604bc250424b988704056e
Amila Dimantha <amiladimantha123@gmail.com>
2023-10-09 15:05:23 +0530

    blog updates

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -6,17 +6,18 @@ import './blog.css';
 const Blog = () => (
   <div className="gpt3__blog section__padding" id="blog">
     <div className="gpt3__blog-heading">
-      <h1 className="gradient__text">A lot is happening, <br /> We are blogging about it.</h1>
+      <h1 className="gradient__text">There's much going on, and we're actively writing blog posts about it.</h1>
     </div>
     <div className="gpt3__blog-container">
       <div className="gpt3__blog-container_groupA">
-        <Article imgUrl={blog01} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+      <Article imgUrl={blog03} date="Sep 12, 2023" text="Join us for OpenAI’s first developer conference on November 6 in San Francisco" link="https://openai.com/blog/announcing-openai-devday"/>
+
       </div>
       <div className="gpt3__blog-container_groupB">
-        <Article imgUrl={blog02} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-        <Article imgUrl={blog03} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-        <Article imgUrl={blog04} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-        <Article imgUrl={blog05} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog02} date="Sep 26, 2023" text="OpenAI Red Teaming Network" link="https://openai.com/blog/red-teaming-network"  />
+        <Article imgUrl={blog01} date="Sep 26, 2023" text="Introducing OpenAI Dublin" link='https://openai.com/blog/introducing-openai-dublin' />
+        <Article imgUrl={blog04} date="Sep 06, 2023" text="Teaching with AI" link="https://openai.com/blog/teaching-with-ai"/>
+        <Article imgUrl={blog05} date="Aug 26, 2023" text="Introducing OpenAI London" link="https://openai.com/blog/introducing-openai-london" />
       </div>
     </div>
   </div>
